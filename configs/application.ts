480a46bf65ce137d5cbbc56a670dc9a2bc63bc27
qiyuan <wangqiyuan01@bilibili.com>
2020-12-28 19:11:37 +0800

    init

---
+++ configs/application.ts
@@ -0,0 +1,20 @@
+import 'reflect-metadata';
+import Koa from 'koa';
+import { Container } from 'typedi';
+import { routingConfigs } from './routing.options';
+import { useMiddlewares } from './koa.middlewares';
+import { useKoaServer, useContainer } from 'routing-controllers';
+
+const createServer = async (): Promise<Koa> => {
+  const koa: Koa = new Koa();
+
+  useMiddlewares(koa);
+
+  const app: Koa = useKoaServer<Koa>(koa, routingConfigs);
+
+  useContainer(Container);
+
+  return app;
+};
+
+export default createServer;
---
+++ configs/environments/development.ts
@@ -0,0 +1,12 @@
+export default {
+  identity: 'development',
+
+  port: 3000,
+
+  DB_TYPE: 'mongodb',
+  MY_HOST: '127.0.0.1',
+  MY_PORT: 27017,
+  USERNAME: 'admin',
+  PASSWORD: 'admin',
+  DATABASE: 'koa_ex_db',
+};
